276b78bf4998d515f559bb6bb78aa8cde4d97208
Kshitij :) <156404280+kshitij-bhale@users.noreply.github.com>
2025-07-19 19:30:00 +0530

    hawao: Added motocam

--- device.mk
+++ device.mk
@@ -38,6 +38,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.nfc@1.2-service.samsung
 
+# Moto Camera 4
+TARGET_MOTCAMERA4 := hawao
+TARGET_USES_MOTCAMERA4 := true
+$(call inherit-product, vendor/motorola/MotCamera4-bengal/motcamera4.mk)
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/nfc/libnfc-sec-vendor.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-sec-vendor.conf
 
