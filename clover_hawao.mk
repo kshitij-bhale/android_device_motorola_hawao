d4716a8b1af45695b628c8a86f3a67306286fc5f
Kshitij Bhale <kbhale01@outlook.com>
2025-11-18 09:05:39 +0530

    hawao: BringUp for theCloverPorject

--- clover_hawao.mk
+++ clover_hawao.mk
@@ -10,11 +10,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from device
 $(call inherit-product, device/motorola/hawao/device.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common The Clover Project stuff.
+$(call inherit-product, vendor/clover/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_hawao
+PRODUCT_NAME := clover_hawao
 PRODUCT_DEVICE := hawao
 PRODUCT_BRAND := motorola
 PRODUCT_MODEL := moto g42
